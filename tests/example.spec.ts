60f415b071af4ea581b4b23ffbbc9f81b75d2913
SPMTD <poolsander97@gmail.com>
2026-01-12 10:55:43 +0100

    Added PUT example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,13 @@
 import { expect, test } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
+/**
+ * Example API test
+ * Including:
+ * GET
+ * POST
+ * DELETE
+ */
 test("Create and delete news article ", async ({request}) => {
   const tokenResponse = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
     data: {"user": { "email": "testAPIuser_Valori@test.com", "password": "testAPIuser_Valori"} }
@@ -56,4 +63,79 @@ test("Create and delete news article ", async ({request}) => {
   });
 
   expect(validateTeardown.status()).toEqual(404);
+});
+
+/**
+ * Example API test
+ * Including 
+ * GET, POST, PUT, DELETE
+ */
+test("Create, update and delete news article ", async ({request}) => {
+  const tokenResponse = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {"user": { "email": "testAPIuser_Valori@test.com", "password": "testAPIuser_Valori"} }
+  });
+
+  const tokenResponseJSON = await tokenResponse.json();
+  const authToken = `Token ${tokenResponseJSON.user.token}`
+
+  const newsArticleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles', {
+    data: {
+      "article" : {
+        "title" : `Test PUT Article-${faker.number.int(99999)}`,
+        "description" : `Test Description: ${faker.lorem.paragraph()}`,
+        "body": `Test body: ${faker.lorem.paragraph(5)}`,
+        "tagList": []
+      }
+    },
+    headers: {
+      Authorization: authToken
+    }
+  });
+
+  const newsArticleResponseJSON = await newsArticleResponse.json();
+  expect(newsArticleResponse.status()).toEqual(201);
+  expect(newsArticleResponseJSON.article.title).toContain("Test PUT Article")
+  
+  const slugId = newsArticleResponseJSON.article.slug;
+
+  //Use authToken to login to make sure the first article is not the standard bondaracademy.
+  const articlesResponse = await request.get('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', {
+    headers: {
+      Authorization: authToken
+    }
+  });
+
+  const articleResponseJson = await articlesResponse.json();
+  expect(articlesResponse.status()).toEqual(200);
+  expect(articleResponseJson.articles[0].title).toContain("Test PUT Article")
+
+  //PUT
+  const updateArticle = await request.put(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    data: {
+      "article" : {
+        "title" : `Test PUT Article Updated-${faker.number.int(99999)}`,
+        "description" : `Test Description: ${faker.lorem.paragraph()}`,
+        "body": `Test body: ${faker.lorem.paragraph(5)}`,
+        "tagList": []
+      }
+    },
+    headers: {
+      Authorization: authToken
+    }
+  });
+
+  //Validate article is updated
+  const updateArticleResponseJSON = await updateArticle.json();
+  const newSlugId = updateArticleResponseJSON.article.slug;
+  expect(updateArticle.status()).toEqual(200);
+  expect(updateArticleResponseJSON.article.title).toContain("Test PUT Article Updated")
+  expect(updateArticleResponseJSON.article.slug).toContain(newSlugId)
+
+  //Teardown
+  const deleteUpdatedArticle = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${newSlugId}`, {
+    headers: {
+      Authorization: authToken
+    }
+  });
+  expect(deleteUpdatedArticle.status()).toEqual(204);
 });
