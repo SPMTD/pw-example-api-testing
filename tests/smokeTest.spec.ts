8231d761fe35c86ebda53aa0b1a886bfba79f11b
SPMTD <poolsander97@gmail.com>
2026-01-15 14:41:37 +0100

    Set up request payload management

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -1,103 +1,78 @@
 import { expect } from "../utils/custom-expect";
 import { test } from "../utils/fixtures";
 import { faker } from "@faker-js/faker";
+import articleRequestPayload from "../request-objects/POST_article.json" with { type: "json" };
 
-test('Get Articles', async({ api }) => {
-    const response = await api
-        .path('/articles')
-        .params({ limit: 10, offset: 0})        
-        .getRequest(200);
+test("Get Articles", async ({ api }) => {
+	const response = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
 
-    await expect(response).shouldMatchSchema('articles', 'GET_articles', true);
-    expect(response.articles.length).shouldBeLessThanOrEqual(10);
-    expect(response.articlesCount).shouldEqual(10);
-}); 
-
-test('Get Test Tags', async({ api }) => {
-    const response = await api
-        .path("/tags")
-        .getRequest(200);
-    await expect(response).shouldMatchSchema('tags', 'GET_tags', true);
-    expect(response.tags[0]).shouldEqual('Test');
-    expect(response.tags.length).shouldBeLessThanOrEqual(10);
+	await expect(response).shouldMatchSchema("articles", "GET_articles", true);
+	expect(response.articles.length).shouldBeLessThanOrEqual(10);
+	expect(response.articlesCount).shouldEqual(10);
 });
 
-test('Create and Delete article', async({ api }) => {
-    const createArticleResponse = await api
-        .path('/articles')
-        .body({
-            "article" : {
-                "title" : `Test Article-${faker.number.int(99999)}`,
-                "description" : `Test Description: ${faker.lorem.paragraph()}`,
-                "body": `Test body: ${faker.lorem.paragraph(5)}`,
-                "tagList": []
-            }  
-        })
-        .postRequest(201)
-    await expect(createArticleResponse).shouldMatchSchema('articles', 'POST_articles');
-    expect(createArticleResponse.article.title).toContain('Test Article-')
-    const slugId = createArticleResponse.article.slug;
-    
-    const articlesResponse = await api
-        .path('/articles')
-        .params({ limit: 10, offset: 0 })
-        .getRequest(200);
-    expect(articlesResponse.articles[0].title).toContain("Test Article-")
-
-    await api
-        .path(`/articles/${slugId}`)
-        .deleteRequest(204)
-
-    const articlesResponseTwo = await api
-        .path('/articles')
-        .params({ limit: 10, offset: 0 })
-        .getRequest(200);
-    expect(articlesResponseTwo.articles[0].title).not.toContain("Test Article-")
+test("Get Test Tags", async ({ api }) => {
+	const response = await api.path("/tags").getRequest(200);
+	await expect(response).shouldMatchSchema("tags", "GET_tags", true);
+	expect(response.tags[0]).shouldEqual("Test");
+	expect(response.tags.length).shouldBeLessThanOrEqual(10);
 });
 
-test('Create, Update and Delete article', async({ api }) => {
-    const createArticleResponse = await api
-        .path('/articles')
-        .body({
-            "article" : {
-                "title" : `Test Article-${faker.number.int(99999)}`,
-                "description" : `Test Description: ${faker.lorem.paragraph()}`,
-                "body": `Test body: ${faker.lorem.paragraph(5)}`,
-                "tagList": []
-            }  
-        })
-        .postRequest(201)
-    expect(createArticleResponse.article.title).toContain('Test Article-')
-    const slugId = createArticleResponse.article.slug;
-    
-    const updateArticleResponse = await api
-        .path(`/articles/${slugId}`)
-        .body({
-            "article" : {
-                "title" : `Updated Test Article-${faker.number.int(99999)}`,
-                "description" : `Test Description: ${faker.lorem.paragraph()}`,
-                "body": `Test body: ${faker.lorem.paragraph(5)}`,
-                "tagList": []
-            }  
-        })
-        .putRequest(200)
-    await expect(updateArticleResponse).shouldMatchSchema('articles', 'PUT_articles');
-    expect(updateArticleResponse.article.title).toContain("Updated Test Article-");
-    const newSlugId = updateArticleResponse.article.slug;
+test("Create and Delete article", async ({ api }) => {
+	//Arrange payload
+    const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+	articleRequest.article.title + faker.number.int({ min: 0, max: 99999 });
+	articleRequest.article.description + faker.number.int({ min: 0, max: 99999 });
+	articleRequest.article.body + faker.lorem.paragraph();
 
-    const articlesResponse = await api
-        .path('/articles')
-        .params({ limit: 10, offset: 0 })
-        .getRequest(200);
-    expect(articlesResponse.articles[0].title).toContain("Updated Test Article-")
+    //Post request
+	const createArticleResponse = await api.path("/articles").body(articleRequest).postRequest(201);
+	await expect(createArticleResponse).shouldMatchSchema("articles", "POST_articles");
+	expect(createArticleResponse.article.title).toContain("Test Article-");
+	const slugId = createArticleResponse.article.slug;
 
-    await api
-        .path(`/articles/${newSlugId}`)
-        .deleteRequest(204)
+    //Validate
+	const articlesResponse = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
+	expect(articlesResponse.articles[0].title).toContain("Test Article-");
 
-    const articlesResponseTwo = await api
-        .path('/articles')
-        .params({ limit: 10, offset: 0 })
-        .getRequest(200);
-    expect(articlesResponseTwo.articles[0].title).not.toContain("Updated Test Article-")
+    //Delete
+	await api.path(`/articles/${slugId}`).deleteRequest(204);
+
+    //Validate deletion (.not example)
+	const articlesResponseTwo = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
+	expect(articlesResponseTwo.articles[0].title).not.toContain("Test Article-");
+});
+
+test("Create, Update and Delete article", async ({ api }) => {
+	//Arrange payload
+	const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+	articleRequest.article.title + faker.number.int({ min: 0, max: 99999 });
+	articleRequest.article.description + faker.number.int({ min: 0, max: 99999 });
+	articleRequest.article.body + faker.lorem.paragraph();
+
+    //POST request
+	const createArticleResponse = await api.path("/articles").body(articleRequest).postRequest(201);
+	expect(createArticleResponse.article.title).toContain("Test Article-");
+	const slugId = createArticleResponse.article.slug;
+
+    //Update article title
+	articleRequest.article.title =
+		"Updated " + articleRequest.article.title + faker.number.int({ min: 0, max: 99999 });
+
+    //PUT updated request
+	const updateArticleResponse = await api.path(`/articles/${slugId}`).body(articleRequest).putRequest(200);
+	await expect(updateArticleResponse).shouldMatchSchema("articles", "PUT_articles");
+	expect(updateArticleResponse.article.title).toContain("Updated Test Article-");
+	const newSlugId = updateArticleResponse.article.slug;
+
+    //Validate 
+	const articlesResponse = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
+	expect(articlesResponse.articles[0].title).toContain("Updated Test Article-");
+
+    //Delete
+	await api.path(`/articles/${newSlugId}`).deleteRequest(204);
+
+    //Validate deletion
+	const articlesResponseTwo = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
+	expect(articlesResponseTwo.articles[0].title).not.toContain("Updated Test Article-");
 });
--- utils/schema-validator.ts
+++ utils/schema-validator.ts
@@ -1,47 +1,52 @@
-import fs from 'fs/promises';
-import path from 'path';
-import Ajv from 'ajv';
-import { createSchema } from 'genson-js';
+import fs from "fs/promises";
+import path from "path";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import { createSchema } from "genson-js";
 
-const SCHEMA_BASE_PATH = './response-schemas';
-const ajv = new Ajv({allErrors: true});
+const SCHEMA_BASE_PATH = "./response-schemas";
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
 
+export async function validateSchema(
+	dirName: string,
+	fileName: string,
+	responseBody: object,
+	createSchemaFlag: boolean = false
+) {
+	const schemaPath = path.join(SCHEMA_BASE_PATH, dirName, `${fileName}_schema.json`);
 
-export async function validateSchema(dirName:string, fileName: string, responseBody: object, createSchemaFlag: boolean = false) {
-    const schemaPath = path.join(SCHEMA_BASE_PATH, dirName, `${fileName}_schema.json`);
+	if (createSchemaFlag) await generateNewSchema(responseBody, schemaPath);
 
-    if (createSchemaFlag) await generateNewSchema(responseBody, schemaPath)
+	const schema = await LoadSchema(schemaPath);
+	const validate = ajv.compile(schema);
 
-    const schema = await LoadSchema(schemaPath);
-    const validate = ajv.compile(schema);
-
-    const valid = validate(responseBody);
-    if (!valid) {
-        throw new Error(
-            `Schema validation failed: ${fileName}_schema.json Failed:\n` +
-            `${JSON.stringify(validate.errors, null, 4)}\n\n` +
-            `Actual response body: \n` +
-            `${JSON.stringify(responseBody, null, 4)}`
-        );
-    };
-    
+	const valid = validate(responseBody);
+	if (!valid) {
+		throw new Error(
+			`Schema validation failed: ${fileName}_schema.json Failed:\n` +
+				`${JSON.stringify(validate.errors, null, 4)}\n\n` +
+				`Actual response body: \n` +
+				`${JSON.stringify(responseBody, null, 4)}`
+		);
+	}
 }
 
-async function LoadSchema(schemaPath:string) {
-    try {
-        const schemaContent = await fs.readFile(schemaPath, 'utf-8');
-        return JSON.parse(schemaContent);
-    } catch (error) {
-        throw new Error(`Failed to read the schema file: ${error.message}`);
-    }    
+async function LoadSchema(schemaPath: string) {
+	try {
+		const schemaContent = await fs.readFile(schemaPath, "utf-8");
+		return JSON.parse(schemaContent);
+	} catch (error) {
+		throw new Error(`Failed to read the schema file: ${error.message}`);
+	}
 }
 
 async function generateNewSchema(responseBody: object, schemaPath: string) {
-    try {
-        const generatedSchema = createSchema(responseBody);
-        await fs.mkdir(path.dirname(schemaPath), {recursive: true});
-        await fs.writeFile(schemaPath, JSON.stringify(generatedSchema, null, 4));
-    } catch (error) {
-        throw new Error(`Failed to create schema file: ${error.message}`);
-    }
+	try {
+		const generatedSchema = createSchema(responseBody);
+		await fs.mkdir(path.dirname(schemaPath), { recursive: true });
+		await fs.writeFile(schemaPath, JSON.stringify(generatedSchema, null, 4));
+	} catch (error) {
+		throw new Error(`Failed to create schema file: ${error.message}`);
+	}
 }
